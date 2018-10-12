77c46687cbee3b16078f58940e8594bed338c3c0
Joe Corr <jcorr@cpbgroup.com>
2018-10-12 17:14:12 -0600

    debugging v2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use('/_assets', express.static(__dirname + '/_assets'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-app.use(partials());
+
 
 
 // load in the json file for creating dynamic pages
