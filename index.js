dd81711beb14bc601da6707c42a494809f0773da
Joe Corr <jcorr@cpbgroup.com>
2018-10-12 17:50:30 -0600

    removing old files

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const port = process.env.PORT || 3000;
 const app = express();
 const util = require('util')
 
-
-
 app.use('/_assets', express.static(__dirname + '/_assets'));
 
 // set the view engine to ejs
@@ -15,47 +13,6 @@ app.set('view engine', 'ejs');
 // load in the json file for creating dynamic pages
 var fs = require('fs');
 var content = JSON.parse(fs.readFileSync("pages.json", 'utf8'));
-
-/*
-var content =
-{
-  "item1": {
-    "title": "Price Per Like",
-    "h1": "Book By Price Per Like",
-    "h2": "The Best Hotel For Social Pics",
-    "desc": "Up your photo game. Now you'll know the price per night AND price per 'like'.",
-    "url": "PricePerLike"
-  },
-  "item2": {
-    "title": "Follwers Deserve",
-    "h1": "Reward your social followers.",
-    "h2": "Take the trip they want to see",
-    "desc": "Because we're all doing it for the 'likes'. Especially when it comes to travel. ",
-    "url": "TripFollowersDeserve"
-  },
-  "item3": {
-    "title": "Hate-Like",
-    "h1": "Your followers hate-like you",
-    "h2": "Make them jealous of your trip",
-    "desc": "We're a travel company dedicated to helping you make them seethe with envy.",
-    "url": "TravelJealousy"
-  },
-  "item4": {
-    "title": "Ego",
-    "h1": "Boost Your Ego w/ Social Likes",
-    "h2": "Book an 'Ego Trip' with Us!",
-    "desc": "This travel company wants you to take an ego trip you can't get anywhere else.",
-    "url": "TravelEgoTrip"
-  },
-  "item5": {
-    "title": "Be There Get Rewarded.",
-    "h1": "Do obvious travel things",
-    "h2": "And get rewaded for it. ",
-    "desc": "We're a travel company that celebrates making obvious travel choices!",
-    "url": "TravelObvious"
-  }
-}
-*/
 
 const entries = Object.entries(content);
 
@@ -78,15 +35,6 @@ app.get("/", function(req, res) {
     	pagecontent: cc
     });
 });
-
-app.get("/confirmation", function(req, res) {
-	res.render('pages/confirmation', {
-    	pagecontent: {"title": "Confirmation"}
-    });
-});
-
-
-
 
 
 app.listen(port, () => console.log(`CPB TestTestTestDotCom listening on port ${port}!`))
