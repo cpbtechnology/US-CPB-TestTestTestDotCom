bb1cbed10e69c6da20040fb2773c9a624a6103fc
Joe Corr <jcorr@cpbgroup.com>
2018-10-12 16:25:08 -0600

    Updated to include dynamic content

---
+++ _json/pages.js
@@ -0,0 +1,18 @@
+{
+  'item1': 
+  	{
+  		title	: "Price Per Like",
+		h1		: "Book By Price Per Like",
+		h2		: "The Best Hotel For Social Pics",
+		desc	: "Up your photo game. Now you'll know the price per night AND price per 'like'.",
+		url 	: "PricePerLike"
+	},
+  'item2': 
+  	{
+  		title	: "Follwers Deserve",
+		h1		: "Reward your social followers.",
+		h2		: "Take the trip they want to see",
+		desc 	: "Because we're all doing it for the 'likes'. Especially when it comes to travel. ",
+		url		: "TripFollowersDeserve"
+	},
+}
--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+const util = require('util')
+
+
 
 app.use('/_assets', express.static(__dirname + '/_assets'));
-app.get('/', (req, res) => res.sendfile(__dirname + '/index.html'))
-app.get('/confirmation.html', (req, res) => res.sendfile(__dirname + '/confirmation.html'))
+//app.get('/', (req, res) => res.sendfile(__dirname + '/index.html'))
+app.get('/confirmations.html', (req, res) => res.sendfile(__dirname + '/confirmation.html'))
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+// load in the json file for creating dynamic pages
+var fs = require('fs');
+var content = JSON.parse(fs.readFileSync("pages.json", 'utf8'));
+
+
+const entries = Object.entries(content);
+
+for (const [obj, handle] of entries) {
+    // dynamic pages 
+	app.get("/"+handle.url, function(req, res) {
+		    res.render('pages/dynamic', {
+		    	pagecontent: handle,
+		    });
+	});
+}
+
+// our index and confirmation pages need some basic
+var baseitem = {
+    "title": "Testing Platform",
+    //"url": "index.html"
+  };
+
+	app.get("/", function(req, res) {
+		    res.render('pages/index', {
+		    	pagecontent: baseitem,
+		    });
+	});
+
+
+
+
+
 app.listen(port, () => console.log(`CPB TestTestTestDotCom listening on port ${port}!`))
